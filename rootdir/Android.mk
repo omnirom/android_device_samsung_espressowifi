6bfafda808f98b3e540e68c537ee16de47c8d5bc
Andreas Blaesius <skate4life@gmx.de>
2016-02-27 12:32:06 +0100

    espresso: remove espresso/espresso10 distinction on  init.recovery.espresso.rc
    Change-Id: I75d9448409b9017d777044e3701c2435aa15ae8d

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -43,7 +43,6 @@ LOCAL_SRC_FILES    := ../recovery/root/etc/twrp.fstab
 LOCAL_MODULE_PATH  := $(TARGET_RECOVERY_ROOT_OUT)/etc
 include $(BUILD_PREBUILT)
 
-ifneq ($(filter p3100 p3110,$(TARGET_DEVICE)),)
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.recovery.espresso.rc
 LOCAL_MODULE_TAGS  := optional eng
@@ -51,7 +50,6 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := ../recovery/root/init.recovery.espresso.rc
 LOCAL_MODULE_PATH  := $(TARGET_RECOVERY_ROOT_OUT)
 include $(BUILD_PREBUILT)
-endif
 
 ifeq ($(TW_EXCLUDE_DEFAULT_USB_INIT), true)
 include $(CLEAR_VARS)
