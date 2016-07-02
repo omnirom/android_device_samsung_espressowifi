08889b5c6d2de0e53cf5acf1aa096394b37b767b
Andreas Blaesius <skate4life@gmx.de>
2016-07-02 14:04:29 +0200

    More cleanup, reorder files and sync with AOSP
    - move Omni specific defines to a seperate BoardConfigOmni.mk
    
    Change-Id: I1b00e4bef03f4553cc62dc1f5f25a417ffb00866

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -15,15 +15,14 @@
 # limitations under the License.
 #
 
-# This variable is set first, so it can be overridden
-# by BoardConfigVendor.mk
+# Inherit Omni specific board config
+-include device/samsung/espressowifi/BoardConfigOmni.mk
 
 # Inherit common omap4 board config
 -include hardware/ti/omap4/BoardConfigCommon.mk
 
 TARGET_NO_BOOTLOADER := true
 
-TARGET_BOARD_OMAP_CPU := 4430
 TARGET_BOOTLOADER_BOARD_NAME := piranha
 
 # Inline kernel building
@@ -33,7 +32,7 @@ BOARD_NAND_PAGE_SIZE := 4096
 BOARD_NAND_SPARE_SIZE := 128
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_BASE := 0x40000000
-# BOARD_KERNEL_CMDLINE :=
+BOARD_KERNEL_CMDLINE := androidboot.hardware=espresso
 
 # Use dlmalloc
 MALLOC_IMPL := dlmalloc
@@ -57,16 +56,7 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1073741824
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 5003787264
 BOARD_FLASH_BLOCK_SIZE := 4096
 
-# Boot Animation
-TARGET_BOOTANIMATION_USE_RGB565 := true
-
-# Vold
-BOARD_VOLD_MAX_PARTITIONS := 12
-BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
-BOARD_VOLD_DISC_HAS_MULTIPLE_MAJORS := true
-TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/class/android_usb/f_mass_storage/lun%d/file"
-
-# Wifi
+# Wi-Fi
 BOARD_WLAN_DEVICE                := bcmdhd
 WPA_SUPPLICANT_VERSION           := VER_0_8_X
 BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
@@ -80,16 +70,14 @@ WIFI_DRIVER_FW_PATH_AP           := "/system/etc/wifi/bcmdhd_apsta.bin"
 WIFI_DRIVER_FW_PATH_P2P          := "/system/etc/wifi/bcmdhd_p2p.bin"
 WIFI_DRIVER_MODULE_NAME          := "bcmdhd"
 WIFI_DRIVER_MODULE_ARG           := "firmware_path=/system/etc/wifi/bcmdhd_sta.bin nvram_path=/system/etc/wifi/nvram_net.txt"
-WIFI_DRIVER_MODULE_AP_ARG        := "firmware_path=/system/etc/wifi/bcmdhd_apsta.bin nvram_path=/system/etc/wifi/nvram_net.txt"
 WIFI_BAND                        := 802_11_ABG
 
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_BCM := true
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/espressowifi/bluetooth
-BOARD_BLUEDROID_VENDOR_CONF := device/samsung/espressowifi/bluetooth/vnd_espresso.txt
 
-# Selinux
+# SELinux
 BOARD_SEPOLICY_DIRS += \
     device/samsung/espressowifi/sepolicy
 
@@ -112,27 +100,7 @@ BOARD_SEPOLICY_UNION += \
     vold.te
 
 # Recovery
-TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
-BOARD_UMS_LUNFILE := "/sys/class/android_usb/f_mass_storage/lun0/file"
-BOARD_SUPPRESS_EMMC_WIPE := true
-TARGET_RECOVERY_FSTAB := device/samsung/espressowifi/rootdir/etc/fstab.tab2
 RECOVERY_FSTAB_VERSION := 2
-TARGET_RECOVERY_DEVICE_DIRS += device/samsung/espressowifi
-BOARD_CUSTOM_BOOTIMG_MK := device/samsung/espressowifi/custombootimg.mk
-
-# TWRP
-TW_THEME := landscape_hdpi
-HAVE_SELINUX := true
-RECOVERY_SDCARD_ON_DATA := true
-BOARD_HAS_NO_REAL_SDCARD := true
-TW_INTERNAL_STORAGE_PATH := "/data/media"
-TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
-TW_EXTERNAL_STORAGE_PATH := "/external_sd"
-TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
-TW_NO_REBOOT_BOOTLOADER := true
-TW_HAS_DOWNLOAD_MODE := true
-TW_FLASH_FROM_STORAGE := true
-TW_INCLUDE_CRYPTO := true
-TW_MAX_BRIGHTNESS := 255
-TW_NO_CPU_TEMP := true
-TW_EXCLUDE_DEFAULT_USB_INIT := true
+TARGET_RECOVERY_FSTAB := device/samsung/espressowifi/rootdir/etc/fstab.tab2
+TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
+BOARD_SUPPRESS_EMMC_WIPE := true
---
+++ BoardConfigOmni.mk
@@ -0,0 +1,30 @@
+# Boot Animation
+TARGET_BOOTANIMATION_USE_RGB565 := true
+
+# Vold
+BOARD_VOLD_MAX_PARTITIONS := 12
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+BOARD_VOLD_DISC_HAS_MULTIPLE_MAJORS := true
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/class/android_usb/f_mass_storage/lun%d/file"
+
+# Recovery
+BOARD_UMS_LUNFILE := "/sys/class/android_usb/f_mass_storage/lun0/file"
+TARGET_RECOVERY_DEVICE_DIRS += device/samsung/espressowifi
+BOARD_CUSTOM_BOOTIMG_MK := device/samsung/espressowifi/custombootimg.mk
+
+# TWRP
+TW_THEME := landscape_hdpi
+HAVE_SELINUX := true
+RECOVERY_SDCARD_ON_DATA := true
+BOARD_HAS_NO_REAL_SDCARD := true
+TW_INTERNAL_STORAGE_PATH := "/data/media"
+TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
+TW_EXTERNAL_STORAGE_PATH := "/external_sd"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+TW_NO_REBOOT_BOOTLOADER := true
+TW_HAS_DOWNLOAD_MODE := true
+TW_FLASH_FROM_STORAGE := true
+TW_INCLUDE_CRYPTO := true
+TW_MAX_BRIGHTNESS := 255
+TW_NO_CPU_TEMP := true
+TW_EXCLUDE_DEFAULT_USB_INIT := true
