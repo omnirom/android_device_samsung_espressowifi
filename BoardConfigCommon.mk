Unlegacy-Android/android_device_samsung_espresso

rootdir: switch to PRODUCT_COPY_FILES
Now that we don't need symlinks, we can simplify this part.

BoardConfigCommon.mk
@@ -80,5 +80,5 @@ BOARD_SEPOLICY_DIRS += \
 
 # Recovery
 RECOVERY_FSTAB_VERSION := 2
-TARGET_RECOVERY_FSTAB := device/samsung/espresso/rootdir/etc/fstab.tab2
+TARGET_RECOVERY_FSTAB := device/samsung/espresso/rootdir/fstab.espresso
 TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"

omnirom/android_device_samsung_espressowifi

Change-Id: If4ed194e04de7eeaee60929a1e8b906ffe7ed172

BoardConfigCommon.mk
@@ -107,5 +107,5 @@ BOARD_SEPOLICY_UNION += \
 
 # Recovery
 RECOVERY_FSTAB_VERSION := 2
-TARGET_RECOVERY_FSTAB := device/samsung/espressowifi/rootdir/etc/fstab.tab2
+TARGET_RECOVERY_FSTAB := device/samsung/espressowifi/rootdir/fstab.espresso
 TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"